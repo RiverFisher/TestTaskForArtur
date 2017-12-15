51b3cf00da7b5ddb738df9a30fac5de50cf971b1
River Fisher <goldenarius@gmail.com>
2017-12-16 02:51:42 +1000

    Create 2nd_task_(matrix_filling)_OOP_style.php

---
+++ 2nd_task_(matrix_filling)_OOP_style.php
@@ -0,0 +1,52 @@
+<?php
+
+class SpecificMatrix
+{
+    /**
+     * @var
+     */
+    private $matrix;
+
+    /**
+     * @param int $from
+     * @param int $to
+     */
+    public function fillOfUniqueRandom(int $from, int $to)
+    {
+        $this->matrix = [];
+
+        $uniqueNumbers = [];
+
+        for ($i = 0; $i < 6; $i++) {
+            $this->matrix[$i] = [];
+            for ($j = 0; $j < 6; $j++) {
+                do {
+                    $value = rand(0, 100);
+                } while (in_array($value, $uniqueNumbers));
+                $this->matrix[$i][$j] = $value;
+                array_push($uniqueNumbers, $value);
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public function print()
+    {
+        print_r($this->matrix);
+    }
+
+    /**
+     *
+     */
+    public function clear() {
+        $this->matrix = null;
+    }
+}
+
+$matrix = new SpecificMatrix();
+$matrix->fillOfUniqueRandom(0, 100);
+$matrix->print();
+$matrix->clear();
+$matrix->print();
